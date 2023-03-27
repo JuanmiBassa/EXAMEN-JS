79e304d214c2dc71ea1364d54f1d96fe45babc57
JuanmiBassa <juanmi7633@gmail.com>
2023-03-27 19:02:02 +0200

    commit 7

--- index.js
+++ index.js
@@ -30,29 +30,58 @@ btnNetejaLlista.onclick = netejaLlista;
 
 //Exercici 1: suma d'hores
 function calculaHora() {
-    
+    var horaDividida1 = inputTime1.value.split(':');
+    var horaDividida2 = inputTime2.value.split(':');
+    if (horaDividida1[0] == "" || horaDividida1[1] == "" || horaDividida2[0] == "" || horaDividida2[1] == "") {
+        resultatTime.innerHTML = `Faltan valores.`;
+        resultatTime.style.color = "red";
+    } else {
+        var horas = parseInt(horaDividida1[0]) + parseInt(horaDividida2[0]);
+        var minutos = parseInt(horaDividida1[1]) + parseInt(horaDividida2[1]);
+        if (minutos > 59) {
+            minutos = minutos % 60;
+            horas += 1;
+        }
+        if (minutos < 10) {
+            minutos = "0" + minutos;
+        }
+        if (horas < 10) {
+            horas = "0" + horas;
+        }
+        resultatTime.innerHTML = `${horas}:${minutos}`;
+    }
 }
 
 function netejaEx1() {
-    
+    resultatTime.innerHTML = "";
 }
 
 // Exercici 2: Arrays
 function generaRandomArray() {
-    
+    var numeros = [];
+    var NumMayor = 0;
+    for (let i = 0; i < 10; i++) {
+        var numeroAleatorio = Math.floor(Math.random() * 100);
+        numeros[i] = numeroAleatorio;
+        if (condition) {
+            
+        }
+    }
+    var numMaximo = Math.max(numeros);
+    resultatArray.innerHTML = `Array: ${numeros}, Max: ${numMaximo}, Posición:`;
 }
 
 function netejaEx2() {
-    
+
 }
 
 // Exercici 3: Estils CSS
 function canviaEstil() {
-    
+
 }
 
 function netejaEstil() {
-    
+
 }
 
 // Exercici 4: JSON
@@ -69,10 +98,10 @@ function carregaJSON() {
         });
 }
 
-function pintaDadesJSON(data){
-    
+function pintaDadesJSON(data) {
+
 }
 
-function netejaLlista(){
-   
+function netejaLlista() {
+
 }
